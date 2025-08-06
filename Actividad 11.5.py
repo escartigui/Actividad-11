69a27764b96aea23112468ac61c0040fbeeeb3b1
escartigui <escarlethmayali@gmail.com>
2025-08-06 01:13:58 -0600

    scar

--- Actividad 11.5.py
+++ Actividad 11.5.py
@@ -24,8 +24,8 @@ def menu():
                 if Id in Empleados:
                  print("Ya utilizado")
                 else:
-                 Empleados[Id] = {}
                  break
+            Empleados[Id] = {}
             Empleados[Id]['Nombre'] = input("Ingrese nombre: ")
             Empleados[Id]['Edad'] = int(input("Ingrese edad: "))
             Empleados[Id]['Sexo'] = input("Ingrese sexo: ")
@@ -38,8 +38,8 @@ def menu():
                  if codigo in Empleados[Id]:
                     print("Dato ya ingresado")
                  else:
-                    Empleados[Id][codigo] = {}
                     break
+             Empleados[Id][codigo] = {}
              nombrededi = input("Ingrese nombre: ")
      if op == 2:
         print("\nLISTADO")
@@ -53,9 +53,8 @@ def menu():
             print(f"Edad: {datos['Edad']}")
             print(f"Sexo: {datos['Sexo']}")
             print(f"Ocupación")
-            for cod,nom in datos['Ocupaciones'].items():
-                print(f"codigo{cod}")
-                print(f"nombre{nom}")
+            print(f"Codigo: {codigo}")
+            print(f"Nombre: {nombrededi}")
      if op == 3:
         print("Hasta que nos volvamos a ver")
         break
